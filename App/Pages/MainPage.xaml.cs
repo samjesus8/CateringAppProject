d72006ad120301e5850eab6c1cc412aae20c840e
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-02-27 02:01:20 +0000

    Moved relavant pages to separate folder

---
+++ App/Pages/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnLoginClicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            DisplayAlert("Login Information", $"Username: {username}\nPassword: {password}", "OK");
+        }
+    }
+}
