8172186fb1d3131563eb4f3213b0779247320081
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-04-24 19:11:54 +0100

    [PROGRESS] About Section Implemented #4 Also improved layout of Add/Edit item pages

--- App/Pages/AboutPage.xaml.cs
+++ App/Pages/AboutPage.xaml.cs
@@ -2,10 +2,17 @@ namespace App.Pages;
 
 public partial class AboutPage : ContentPage
 {
+    public string DeviceName { get; set; }
+    public string DeviceOS { get; set; }
+    public string DeviceManufacturer { get; set; }
 	public AboutPage()
 	{
 		InitializeComponent();
-	}
+        this.DeviceName = DeviceInfo.Name;
+        this.DeviceManufacturer = DeviceInfo.Manufacturer;
+        this.DeviceOS = $"{DeviceInfo.Platform} {DeviceInfo.VersionString}";
+        BindingContext = this;
+    }
 
     private async void LearnMore_Clicked(object sender, EventArgs e)
     {
