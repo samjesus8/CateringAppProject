2cb5d0b88db737846d67624abeb5ce8c50d941e0
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-04-29 00:12:45 +0100

    Fixed ShellContent routes for each tab

--- App/AppShell.xaml.cs
+++ App/AppShell.xaml.cs
@@ -11,7 +11,7 @@
         private void AppShell_Navigating(object? sender, ShellNavigatingEventArgs e)
         {
             // Check if the user is pressing admin without being logged in
-            if (e.Target.Location.OriginalString == "//D_FAULT_ShellContent4" && MauiProgram.currentlyLoggedInUser.UserType == null)
+            if (e.Target.Location.OriginalString == "//AdminPage" && MauiProgram.currentlyLoggedInUser.UserType == null)
             {
                 e.Cancel();
 
@@ -19,7 +19,7 @@
             }
 
             // Check if a normal user is trying to press the admin button
-            else if (e.Target.Location.OriginalString == "//D_FAULT_ShellContent4" && MauiProgram.currentlyLoggedInUser.UserType != "admin")
+            else if (e.Target.Location.OriginalString == "//AdminPage" && MauiProgram.currentlyLoggedInUser.UserType != "admin")
             {
                 // Cancel the navigation
                 e.Cancel();
