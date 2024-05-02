83bcc8f5cfdc84ebd0ef3d652582ddda067b6620
Samuel Jesuthas <samueljesuthas27@gmail.com>
2024-05-02 20:04:54 +0100

    [PROGRESS] Implemented way to change account details

--- App/Pages/Account/AccountPage.xaml.cs
+++ App/Pages/Account/AccountPage.xaml.cs
@@ -4,10 +4,12 @@ namespace App.Pages.Account;
 
 public partial class AccountPage : ContentPage
 {
-	public User currentUser { get; set; }
+    private readonly AppDatabaseEngine databaseEngine;
+    public User currentUser { get; set; }
 	public AccountPage()
 	{
 		InitializeComponent();
+		this.databaseEngine = new AppDatabaseEngine();
 
 		//Set current user to the currently logged in user
 		this.currentUser = MauiProgram.currentlyLoggedInUser;
@@ -25,6 +27,28 @@ public partial class AccountPage : ContentPage
 
 	private async void OnChangeClicked(object sender, EventArgs e)
 	{
+		if (!string.IsNullOrWhiteSpace(this.UsernameEntry.Text) || !string.IsNullOrWhiteSpace(this.PasswordEntry.Text) || !string.IsNullOrWhiteSpace(this.AddressEntry.Text))
+		{
+			MauiProgram.currentlyLoggedInUser.Username = this.UsernameEntry.Text;
+			MauiProgram.currentlyLoggedInUser.Password = this.PasswordEntry.Text;
+			MauiProgram.currentlyLoggedInUser.Address = this.AddressEntry.Text;
 
+			var isModified = await databaseEngine.ModifyUserAsync(MauiProgram.currentlyLoggedInUser);
+
+			if (isModified.Item1 == true)
+			{
+				await DisplayAlert("Successfully changed account details", "", "OK");
+				this.PasswordEntry.Text = string.Empty;
+			}
+            else
+            {
+                await DisplayAlert("Something went wrong", $"{isModified.Item2}", "OK");
+            }
+        }
+
+		else
+		{
+			await DisplayAlert("Error", "Please ensure all fields are not empty!!!", "OK");
+		}
 	}
 }
